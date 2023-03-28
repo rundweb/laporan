e77e12eec78f8f490740cccda331df387e0cfa3f
Rifaldo <pn4066283@gmail.com>
2023-03-28 15:30:23 +0700

    new

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const inputTanggal = document.querySelector('#tanggal'),
     inputpengeluaran = document.querySelector('#pengeluaran'),
     inputJenis = document.querySelector('#jenis'),
-    inputKeterngan = document.querySelector('#keterangan')
+    inputKeterngan = document.querySelector('#keterangan'),
+    editTanggal = document.querySelector('#tanggal-edit'),
+    editJenis = document.querySelector('#jenis-edit'),
+    editPengeluaran = document.querySelector('#pengeluaran-edit'),
+    editKeterangan = document.querySelector('#keterangan-edit')
 
 const btnTambah = document.querySelector('.btn-tambah'),
     btnClose = document.querySelector('.close')
 
 const data = []
 
+const namaBulan = ['Januari', 'Februari', 'Maret', 'Aprin', 'Mei', 'Juni', 'July', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
 // btnDetail.addEventListener('click',e=>{
 //     e.preventDefault()
 //     document.querySelector('.popup').style.display = 'flex'
@@ -15,7 +20,10 @@ const data = []
 
 btnClose.addEventListener('click', () => {
     document.querySelector('.popup').style.display = 'none'
+    clearInput()
 })
+
+
 
 function showData() {
     document.querySelectorAll('.datas').forEach(d => d.remove())
@@ -28,68 +36,180 @@ function showData() {
               <td>${data[i].tanggal}</td>
               <td>${data[i].jenis}</td>
               <td>
-                <button class="btn-detail" onclick="btnDetail(${i},${tanggalTime},${data[i].jenis},${data[i].pengeluaran},${data[i].keterangan})">
-                  <i class="ri-folder-5-line"></i>
-                </button>
+                <button class="btn-detail" onclick='btnAksi(${i},${tanggalTime},"${data[i].jenis}",${data[i].pengeluaran},"${data[i].keterangan}")'><i class="ri-folder-5-line"></i></button>
+                  
               </td>
             </tr>
        `
-
-        document.querySelector('tbody').innerHTML += tagTable
+        document.querySelector('.tbody').innerHTML += tagTable
     }
 }
-
 showData()
 
-function btnDetail(id,tanggal,jenis,pengeluaran,keterangan){
+
+function btnAksi(id, tanggal, jenis, pengeluaran, keterangan) {
     document.querySelector('.popup').style.display = 'flex'
-    const tanggalEdit = document.querySelector('#tanggal-edit'),
-     pengeluranEdit = document.querySelector('#pengeluaran-edit'),
-     jenisEdit = document.querySelector('#jenis-edit'),
-     keteranganEdit = document.querySelector('#keterangan-edit')
+    let ubahTanggal = new Date(tanggal).toISOString().split('T')[0]
+    editTanggal.value = ubahTanggal
+    editJenis.value = jenis
+    editPengeluaran.value = pengeluaran
+    editKeterangan.value = keterangan
 
-    let ubahTanggal = new Date(tanggal)
+    let tagBtn = `
+            <button class="btn-edit" onclick='btnEdit(${id},${ubahTanggal})'>Edit</button>
+            <button class="btn-hapus" onclick='btnHapus(${id})'>Hapus</button>
+    `
 
-    console.log(id,tanggal,jenis,pengeluaran,keterangan)
+    document.querySelector('.btn').innerHTML = tagBtn
+
 }
 
+function btnHapus(id) {
+    data.splice(id, 1)
+
+    alert('data berhasil di hapus')
+    showData()
+    showDownload()
+    showTotalDownload()
+    clearInput()
+    btnClose.click()
+}
+
+function btnEdit(id, tanggal) {
+    let infoEdit = {
+        tanggal: editTanggal.value,
+        jenis: editJenis.value,
+        pengeluaran: editPengeluaran.value,
+        keterangan: editKeterangan.value
+
+    }
+
+    data[id] = infoEdit
+    alert('data berhasil diubah')
+    btnClose.click()
+    clearInput()
+    showData()
+    showDownload()
+    showTotalDownload()
+
+}
 
 btnTambah.addEventListener('click', e => {
     e.preventDefault()
-
-    const inputTanggal = document.querySelector('#tanggal').value,
-        inputpengeluaran = document.querySelector('#pengeluaran').value,
-        inputJenis = document.querySelector('#jenis').value,
-        inputKeterngan = document.querySelector('#keterangan').value
+    let infoTanggal = inputTanggal.value,
+        infoJenis = inputJenis.value,
+        infoPengeluaran = inputpengeluaran.value,
+        infoKeterangan = inputKeterngan.value
     let infoData = {
-        tanggal: inputTanggal,
-        jenis: inputJenis,
-        pengeluaran: inputpengeluaran,
-        keterangan: inputKeterngan
+        tanggal: infoTanggal,
+        jenis: infoJenis,
+        pengeluaran: infoPengeluaran,
+        keterangan: infoKeterangan
     }
-    if (inputTanggal == '' || inputpengeluaran == '' || inputJenis == '' || inputKeterngan == '') {
+    if (infoTanggal == '' || infoJenis == '' || infoPengeluaran == '' || infoKeterangan == '') {
         alert('Data belum di isi')
         showData()
-    } else if (data.find(d => d.tanggal == inputTanggal)) {
+        showDownload()
+        showTotalDownload()
+
+    } else if (data.find(d => d.tanggal == infoTanggal)) {
         alert('Tanggal Sudah ada')
         showData()
-    } else {
+        showDownload()
+        showTotalDownload()
 
+    } else {
         data.push(infoData)
         alert('data berhasil ditambahkan')
-        clearInput()
         showData()
-        console.log(data)
+        showDownload()
+        showTotalDownload()
+        clearInput()
+
     }
 })
 
+function showDownload() {
+    document.querySelectorAll('.datass').forEach(d => d.remove())
+    for (let i = 0; i < data.length; i++) {
+        let tagTablee = `
+            <tr class='datass'>
+              <td>${i + 1}</td>
+              <td>${data[i].tanggal}</td>
+              <td>${data[i].jenis}</td>
+              <td>${formatRupiah(data[i].pengeluaran)}</td>
+              <td>${data[i].keterangan}</td>
+            </tr>
+       `
+        document.querySelector('.tbody-download').innerHTML += tagTablee
+    }
+}
+showDownload()
 
+function showTotalDownload() {
+    const total = document.querySelector('.total')
+    setInterval(() => {
+        let totalHarga = 0
+        for (let i = 0; i < data.length; i++) {
+            totalHarga += parseInt(data[i].pengeluaran)
+            total.innerHTML = formatRupiah(totalHarga)
+            document.querySelector('.pengeluaran-setoran').innerHTML = formatRupiah(totalHarga)
+        }
+    }, 100);
+    if (data.length === 0) {
+        total.innerHTML = 0
+    }
 
+}
+showTotalDownload()
+
+function myFunc(total, num) {
+    return total - num;
+}
 
 function clearInput() {
     inputTanggal.value = ''
     inputpengeluaran.value = ''
     inputJenis.value = ''
     inputKeterngan.value = ''
+    document.querySelector('.btn').innerHTML = ''
+    editTanggal.value = ''
+    editJenis.value = ''
+    editPengeluaran.value = ''
+    editKeterangan.value = ''
+
+}
+const formatRupiah = (money) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+    }).format(money);
 }
 
+
+
+function download() {
+    var downloaded = document.querySelector('.pengeluaran-download')
+
+    var pangkalan = document.querySelector('#pangkalan')
+    document.querySelector('.nama').innerHTML = pangkalan.value
+    document.querySelector('.pengeluaran-download').style.display = 'flex'
+    let bulan = new Date(),
+        getBulan = namaBulan[bulan.getMonth() - 1]
+
+    document.querySelector('.bulan-lalu').innerHTML = getBulan
+
+    
+
+    var opt = {
+        filename: `Pengeluaran Bulan ${getBulan}`,
+    };
+
+    // New Promise-based usage:
+    html2pdf().set(opt).from(downloaded).save();
+
+    setTimeout(() => {
+        document.querySelector('.pengeluaran-download').style.display = 'none'
+    }, 10);
+}
