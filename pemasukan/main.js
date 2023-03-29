612b1af18478a4d287c63772ea01fbe8ff738f6c
Rifaldo <pn4066283@gmail.com>
2023-03-29 14:27:21 +0700

    aplikasi

---
+++ pemasukan/main.js
@@ -0,0 +1,207 @@
+const inputTanggal = document.querySelector('#tanggal'),
+    inputTabung = document.querySelector('#tabung'),
+    btnTambah = document.querySelector('#btn-tambah'),
+    btnClose = document.querySelector('#close')
+
+const data = []
+const hargaGas = 16000
+const namaBulan = ['Januari', 'Februari', 'Maret', 'Aprin', 'Mei', 'Juni', 'July', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+function showData() {
+    const tBody = document.querySelector('#tBody')
+    document.querySelectorAll('#datas').forEach(datas => datas.remove())
+    for (let i = 0; i < data.length; i++) {
+        let d = new Date(data[i].tanggal)
+        let ubahTanggal = d.getTime()
+        let tagTable = `
+            <tr id='datas'>
+              <td>${i + 1}</td>
+              <td>${data[i].tanggal}</td>
+              <td>${data[i].tabung}</td>
+              <td>${formatRupiah(data[i].harga)}</td>
+              <td>${formatRupiah(data[i].total)}</td>
+              <td><i class="ri-settings-4-fill" onclick='btnAksi(${i},${ubahTanggal},${data[i].tabung})'></i></td>
+            </tr>
+        `
+        tBody.innerHTML += tagTable
+    }
+}
+
+showData()
+
+function showTotal() {
+    const tTabung = document.querySelector('#total-tabung')
+    const tHarga = document.querySelector('#total-harga')
+    setInterval(() => {
+        let totalTabung = 0,
+            totalHarga = 0
+        for (let i = 0; i < data.length; i++) {
+            totalHarga += data[i].total
+            totalTabung += parseInt(data[i].tabung)
+
+            tTabung.innerHTML = totalTabung
+            tHarga.innerHTML = formatRupiah(totalHarga)
+        }
+    }, 100);
+
+    if (data.length === 0) {
+        tTabung.innerHTML = 0
+        tHarga.innerHTML = 0
+    }
+
+}
+showTotal()
+
+function showDownload() {
+    const tBodyDonwload = document.querySelector('#tBody-download')
+    document.querySelectorAll('#datas-download').forEach(datas => datas.remove())
+    for (let i = 0; i < data.length; i++) {
+        let tagTable = `
+            <tr id='datas-download'>
+              <td>${i + 1}</td>
+              <td>${data[i].tanggal}</td>
+              <td>${data[i].tabung}</td>
+              <td>${formatRupiah(data[i].harga)}</td>
+              <td>${formatRupiah(data[i].total)}</td>
+              </tr>
+        `
+        tBodyDonwload.innerHTML += tagTable
+    }
+}
+showDownload()
+
+function showTotalDownload() {
+    const tTabung = document.querySelector('#total-tabung-download')
+    const tHarga = document.querySelector('#total-harga-download')
+    setInterval(() => {
+        let totalTabung = 0,
+            totalHarga = 0
+        for (let i = 0; i < data.length; i++) {
+            totalHarga += data[i].total
+            totalTabung += parseInt(data[i].tabung)
+
+            tTabung.innerHTML = totalTabung
+            tHarga.innerHTML = formatRupiah(totalHarga)
+        }
+    }, 100);
+
+    if (data.length === 0) {
+        tTabung.innerHTML = 0
+        tHarga.innerHTML = 0
+    }
+
+}
+showTotalDownload()
+
+function btnAksi(id, tanggal, tabung) {
+    const editTanggal = document.querySelector('#tanggal-edit'),
+        editTabung = document.querySelector('#tabung-edit')
+    let dt = new Date(tanggal).toISOString().split('T')[0]
+    document.querySelector('.popup').style.display = 'flex'
+    let tagBtn = `
+            <button id="btn-edit" onclick='btnEdit(${id},${tanggal},${tabung})'>Edit</button>
+            <button id="btn-hapus" onclick='btnHapus(${id})'>Hapus</button>
+    `
+    document.querySelector('.btn').innerHTML = tagBtn
+
+    editTanggal.value = dt
+    editTabung.value = tabung
+
+}
+
+function btnHapus(id) {
+    data.splice(id, 1)
+    showData()
+    alert('Data berhasil di hapus')
+    btnClose.click()
+    showTotal()
+    showDownload()
+}
+
+function btnEdit(id, tanggal, tabung) {
+    const tanggalEdit = document.querySelector('#tanggal-edit'),
+        tabungEdit = document.querySelector('#tabung-edit')
+    let infoEdit = {
+        tanggal: tanggalEdit.value,
+        tabung: tabungEdit.value,
+        harga: hargaGas,
+        total: tabungEdit.value * hargaGas
+
+    }
+    alert('data berhasil diubah')
+    data[id] = infoEdit
+    showData()
+    showTotal()
+    showDownload()
+    btnClose.click()
+}
+
+btnTambah.addEventListener('click', e => {
+    e.preventDefault()
+    let infoTabung = inputTabung.value,
+        infoTanggal = inputTanggal.value
+
+    if (infoTabung != '' && infoTanggal != '') {
+        let infoBarang = {
+            tanggal: infoTanggal,
+            tabung: infoTabung,
+            harga: hargaGas,
+            total: infoTabung * hargaGas
+        }
+        if (data.find(data => data.tanggal == infoTanggal)) {
+            alert('Tanggal sudah ada')
+            showData()
+            showTotal()
+            showDownload()
+        } else {
+            data.push(infoBarang)
+            alert('Data berhasil di tambah')
+            clear()
+            showData()
+            showTotal()
+            showDownload()
+        }
+    } else {
+        alert('Tanggal atau jumlah belum di isi')
+        showData()
+        showTotal()
+        showDownload()
+    }
+})
+
+function clear() {
+    inputTabung.value = ''
+    inputTanggal.value = ''
+}
+
+const formatRupiah = (money) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+    }).format(money);
+}
+
+btnClose.addEventListener('click', () => {
+    document.querySelector('.popup').style.display = 'none'
+    document.querySelector('.btn').innerHTML = ''
+})
+
+
+function download() {
+    const element = document.querySelector('.laporan-download'),
+    pangkalan = document.querySelector('#pangkalan')
+    document.querySelector('.nama').innerHTML = pangkalan.value
+    element.style.display = 'flex'
+    let bulan = new Date(),
+        getBulan = namaBulan[bulan.getMonth() - 1]
+    var opt = {
+        filename: `Pemasukan Bulan ${getBulan}`,
+    };
+
+    // New Promise-based usage:
+    html2pdf().set(opt).from(element).save();
+
+        setTimeout(() => {
+            element.style.display = 'none'
+        }, 10);
+}
---
+++ pengeluaran/main.js
@@ -0,0 +1,230 @@
+const inputTanggal = document.querySelector('#tanggal'),
+    inputpengeluaran = document.querySelector('#pengeluaran'),
+    inputJenis = document.querySelector('#jenis'),
+    inputKeterngan = document.querySelector('#keterangan'),
+    editTanggal = document.querySelector('#tanggal-edit'),
+    editJenis = document.querySelector('#jenis-edit'),
+    editPengeluaran = document.querySelector('#pengeluaran-edit'),
+    editKeterangan = document.querySelector('#keterangan-edit')
+
+const btnTambah = document.querySelector('.btn-tambah'),
+    btnClose = document.querySelector('.close')
+
+const data = []
+const saldo = []
+const pemasukan = []
+
+
+
+
+const namaBulan = ['Januari', 'Februari', 'Maret', 'Aprin', 'Mei', 'Juni', 'July', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+// btnDetail.addEventListener('click',e=>{
+//     e.preventDefault()
+//     document.querySelector('.popup').style.display = 'flex'
+// })
+
+btnClose.addEventListener('click', () => {
+    document.querySelector('.popup').style.display = 'none'
+    clearInput()
+})
+
+
+
+function showData() {
+    document.querySelectorAll('.datas').forEach(d => d.remove())
+    for (let i = 0; i < data.length; i++) {
+        let ubahTanggal = new Date(data[i].tanggal)
+        let tanggalTime = ubahTanggal.getTime()
+        let tagTable = `
+            <tr class='datas'>
+              <td>${i + 1}</td>
+              <td>${data[i].tanggal}</td>
+              <td>${data[i].jenis}</td>
+              <td>
+                <button class="btn-detail" onclick='btnAksi(${i},${tanggalTime},"${data[i].jenis}",${data[i].pengeluaran},"${data[i].keterangan}")'><i class="ri-folder-5-line"></i></button>
+                  
+              </td>
+            </tr>
+       `
+        document.querySelector('.tbody').innerHTML += tagTable
+    }
+}
+showData()
+
+
+function btnAksi(id, tanggal, jenis, pengeluaran, keterangan) {
+    document.querySelector('.popup').style.display = 'flex'
+    let ubahTanggal = new Date(tanggal).toISOString().split('T')[0]
+    editTanggal.value = ubahTanggal
+    editJenis.value = jenis
+    editPengeluaran.value = pengeluaran
+    editKeterangan.value = keterangan
+
+    let tagBtn = `
+            <button class="btn-edit" onclick='btnEdit(${id},${ubahTanggal})'>Edit</button>
+            <button class="btn-hapus" onclick='btnHapus(${id})'>Hapus</button>
+    `
+
+    document.querySelector('.btn').innerHTML = tagBtn
+
+}
+
+function btnHapus(id) {
+    data.splice(id, 1)
+
+    alert('data berhasil di hapus')
+    showData()
+    showDownload()
+    showTotalDownload()
+    clearInput()
+    btnClose.click()
+}
+
+function btnEdit(id, tanggal) {
+    let infoEdit = {
+        tanggal: editTanggal.value,
+        jenis: editJenis.value,
+        pengeluaran: editPengeluaran.value,
+        keterangan: editKeterangan.value
+
+    }
+
+    data[id] = infoEdit
+    alert('data berhasil diubah')
+    btnClose.click()
+    clearInput()
+    showData()
+    showDownload()
+    showTotalDownload()
+
+}
+
+btnTambah.addEventListener('click', e => {
+    e.preventDefault()
+    let infoTanggal = inputTanggal.value,
+        infoJenis = inputJenis.value,
+        infoPengeluaran = inputpengeluaran.value,
+        infoKeterangan = inputKeterngan.value
+    let infoData = {
+        tanggal: infoTanggal,
+        jenis: infoJenis,
+        pengeluaran: infoPengeluaran,
+        keterangan: infoKeterangan
+    }
+    if (infoTanggal == '' || infoJenis == '' || infoPengeluaran == '' || infoKeterangan == '') {
+        alert('Data belum di isi')
+        showData()
+        showDownload()
+        showTotalDownload()
+
+    } else if (data.find(d => d.tanggal == infoTanggal)) {
+        alert('Tanggal Sudah ada')
+        showData()
+        showDownload()
+        showTotalDownload()
+
+    } else {
+        data.push(infoData)
+        alert('data berhasil ditambahkan')
+        showData()
+        showDownload()
+        showTotalDownload()
+        clearInput()
+
+    }
+})
+
+function showDownload() {
+    document.querySelectorAll('.datass').forEach(d => d.remove())
+    for (let i = 0; i < data.length; i++) {
+        let tagTablee = `
+            <tr class='datass'>
+              <td>${i + 1}</td>
+              <td>${data[i].tanggal}</td>
+              <td>${data[i].jenis}</td>
+              <td>${formatRupiah(data[i].pengeluaran)}</td>
+              <td>${data[i].keterangan}</td>
+            </tr>
+       `
+        document.querySelector('.tbody-download').innerHTML += tagTablee
+    }
+}
+showDownload()
+
+function showTotalDownload() {
+    const total = document.querySelector('.total')
+    setTimeout(() => {
+        let totalHarga = 0
+        for (let i = 0; i < data.length; i++) {
+            totalHarga += parseInt(data[i].pengeluaran)
+            total.innerHTML = formatRupiah(totalHarga)
+            document.querySelector('.pengeluaran-setoran').innerHTML = formatRupiah(totalHarga)
+
+            if (saldo.length == 0) {
+                saldo.push(totalHarga)
+            }else{
+                saldo.shift()
+                saldo.push(totalHarga)
+            }
+        }
+    }, 100);
+
+    if (data.length === 0) {
+        total.innerHTML = 0
+    }
+
+}
+showTotalDownload()
+
+// menangkap includes
+
+
+function clearInput() {
+    inputTanggal.value = ''
+    inputpengeluaran.value = ''
+    inputJenis.value = ''
+    inputKeterngan.value = ''
+    document.querySelector('.btn').innerHTML = ''
+    editTanggal.value = ''
+    editJenis.value = ''
+    editPengeluaran.value = ''
+    editKeterangan.value = ''
+
+}
+
+
+function download() {
+    var downloaded = document.querySelector('.pengeluaran-download')
+    var pangkalan = document.querySelector('#pangkalan')
+    document.querySelector('.nama').innerHTML = pangkalan.value
+    var gas = document.querySelector('#gas').value
+    let pemasukanGas = gas * 16000
+    pemasukan.push(pemasukanGas)
+    document.querySelector('.setoran').innerHTML = formatRupiah(pemasukan)
+    let jumlah =  pemasukanGas - saldo
+    
+    document.querySelector('.pengeluaran-download').style.display = 'flex'
+
+    document.querySelector('.saldo').innerHTML = formatRupiah(jumlah)
+    let bulan = new Date(),
+        getBulan = namaBulan[bulan.getMonth() - 1]
+    document.querySelector('.bulan-lalu').innerHTML = getBulan
+    var opt = {
+        filename: `Pengeluaran Bulan ${getBulan}`,
+    };
+
+    // New Promise-based usage:
+    html2pdf().set(opt).from(downloaded).save();
+
+    setTimeout(() => {
+        document.querySelector('.pengeluaran-download').style.display = 'none'
+    }, 10);
+}
+
+const formatRupiah = (money) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+    }).format(money);
+}
